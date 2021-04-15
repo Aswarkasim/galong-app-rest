0c0d4e599ec5ac1438ec6272349ddb819bc56708
aswarkasim <aswarkasim@gmail.com>
2021-04-15 08:50:15 +0700

    order hp lagi

--- application/modules/api/models/Api_model.php
+++ application/modules/api/models/Api_model.php
@@ -16,7 +16,7 @@ class Api_model extends CI_Model
 
   function getOrderDriver($id_driver = null)
   {
-    $this->db->select('tbl_order.*, tbl_user.namalengkap, tbl_user.nohp')
+    $this->db->select('tbl_order.*, tbl_user.namalengkap')
       ->join('tbl_user', 'tbl_user.id_user = tbl_order.id_user', 'left')
       ->where('id_driver', $id_driver)
       ->where('is_done', '0')
