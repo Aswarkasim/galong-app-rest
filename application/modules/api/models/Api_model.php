7bfd4d8bbef85335474a09af65d2d82e0deb3e34
aswarkasim <aswarkasim@gmail.com>
2021-04-15 08:49:08 +0700

    order hp lagi

--- application/modules/api/models/Api_model.php
+++ application/modules/api/models/Api_model.php
@@ -20,7 +20,7 @@ class Api_model extends CI_Model
       ->join('tbl_user', 'tbl_user.id_user = tbl_order.id_user', 'left')
       ->where('id_driver', $id_driver)
       ->where('is_done', '0')
-      ->order_by('date_created', 'ASC');
+      ->order_by('tbl_order.date_created', 'ASC');
     return $this->db->get()->result();
   }
 }
